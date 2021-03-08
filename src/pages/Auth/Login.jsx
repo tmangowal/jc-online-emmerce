89d8ed83d1638f17858059a40241562405ed0b12
Theodore Mangowal <tmangowal@gmail.com>
2021-03-09 00:34:35 +0700

    Topic 5: Login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { loginUser } from '../../redux/actions/user';
+import { connect } from 'react-redux'
 
 class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  }
+
+  inputHandler = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({ [name]: value })
+  }
+
   render() {
+    if (this.props.userGlobal.id) {
+      return <Redirect to="/" />
+    }
+    
     return (
       <div className="container">
         <div className="row">
@@ -16,15 +34,18 @@ class Login extends React.Component {
         </div>
         <div className="row mt-5">
           <div className="col-4 offset-4">
+            {
+              this.props.userGlobal.errMsg ? 
+              <div className="alert alert-danger">{this.props.userGlobal.errMsg}</div>
+              : null
+            }
             <div className="card">
               <div className="card-body">
                 <h5 className="font-weight-bold mb-3">Log in</h5>
-                <input placeholder="Full Name" type="text" className="form-control my-2" />
-                <input placeholder="Username" type="text" className="form-control my-2" />
-                <input placeholder="Email" type="text" className="form-control my-2" />
-                <input placeholder="Password" type="password" className="form-control my-2" />
+                <input onChange={this.inputHandler} name="username" placeholder="Username" type="text" className="form-control my-2" />
+                <input onChange={this.inputHandler} name="password" placeholder="Password" type="password" className="form-control my-2" />
                 <div className="d-flex flex-row justify-content-between align-items-center">
-                  <button className="btn btn-primary mt-2">
+                  <button onClick={() => this.props.loginUser(this.state)} className="btn btn-primary mt-2">
                     Login
                   </button>
                   <Link to="/register">Or register</Link>
@@ -38,4 +59,14 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  return {
+    userGlobal: state.user,
+  };
+}
+
+const mapDispatchToProps = {
+  loginUser,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
