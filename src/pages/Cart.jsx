6bc1f3fd3cd77122fb19443b993217cab4326db9
Theodore Mangowal <tmangowal@gmail.com>
2021-03-10 16:55:00 +0700

    Topic 12: Checkout process

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,11 +5,16 @@ import { API_URL } from "../constants/API"
 import { getCartData } from '../redux/actions/cart';
 
 class Cart extends React.Component {
+  state = {
+    isCheckoutMode: false,
+    recipientName: "",
+    address: "",
+    payment: 0,
+  }
 
   deleteCartHandler = (cartId) => {
     Axios.delete(`${API_URL}/carts/${cartId}`)
     .then(() => {
-      alert("Berhasil delete item dari cart")
       this.props.getCartData(this.props.userGlobal.id)
     })
     .catch(() => {
@@ -46,13 +51,76 @@ class Cart extends React.Component {
     })
   }
 
+  renderSubtotalPrice = () => {
+    let subtotal = 0;
+    for(let i = 0; i < this.props.cartGlobal.cartList.length; i++) {
+      subtotal += this.props.cartGlobal.cartList[i].price * this.props.cartGlobal.cartList[i].quantity
+    }
+
+    return subtotal;
+  }
+
+  renderTaxFee = () => {
+    return this.renderSubtotalPrice() * 0.05;
+  }
+
+  renderTotalPrice = () => {
+    return this.renderSubtotalPrice() + this.renderTaxFee();
+  }
+
+  checkoutModeToggle = () => {
+    this.setState({ isCheckoutMode: !this.state.isCheckoutMode })
+  }
+
+  inputHandler = (event) => {
+    const { name, value } = event.target
+
+    this.setState({ [name]: value });
+  }
+
+  payBtnHandler = () => {
+    // 1. POST ke /transactions
+    // 2. DELETE semua cart item yg sudah dibayar
+
+    if (this.state.payment < this.renderTotalPrice()) {
+      alert(`Uang anda kurang ${this.renderTotalPrice() - this.state.payment}`)
+      return;
+    }
+
+    if (this.state.payment > this.renderTotalPrice()) {
+      alert(`Sukses anda mendapatkan kembalian ${this.state.payment - this.renderTotalPrice()}`)
+    } else if (this.state.payment === this.renderTotalPrice()) {
+      alert("Terima kasih sudah membayar dengan uang pas")
+    }
+
+    const d = new Date();
+    Axios.post(`${API_URL}/transactions`, {
+      userId: this.props.userGlobal.id,
+      address: this.state.address,
+      recipientName: this.state.recipientName,
+      totalPrice: parseInt(this.renderTotalPrice()),
+      totalPayment: parseInt(this.state.payment),
+      transactionDate: `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`, // DD-MM-YYYY
+      transactionItems: this.props.cartGlobal.cartList, // Array of objects -> cart
+    })
+    .then((result) => {
+      alert("Berhasil melakukan pembayaran")
+      result.data.transactionItems.forEach((val) => {
+        this.deleteCartHandler(val.id)
+      })
+    })
+    .catch(() => {
+      alert("Terjadi kesalahan di server")
+    })
+  }
+
   render() {
     return (
-      <div className="p-5">
-        <div className="row">
-          <div className="col-12 text-center">
-            <h1>Cart</h1>
-            <table className="table mt-4">
+      <div className="p-5 text-center">
+        <h1>Cart</h1>
+        <div className="row mt-5">
+          <div className="col-9 text-center">
+            <table className="table">
               <thead className="thead-light">
                 <tr>
                   <th>Name</th>
@@ -69,7 +137,7 @@ class Cart extends React.Component {
               <tfoot className="bg-light">
                 <tr>
                   <td colSpan="6">
-                    <button className="btn btn-success">
+                    <button onClick={this.checkoutModeToggle} className="btn btn-success">
                       Checkout
                     </button>
                   </td>
@@ -77,6 +145,44 @@ class Cart extends React.Component {
               </tfoot>
             </table>
           </div>
+          {
+            this.state.isCheckoutMode ? 
+            <div className="col-3">
+              {/* Form checkout */}
+              <div className="card text-left">
+                <div className="card-header">
+                  <strong>Order Summary</strong>
+                </div>
+                <div className="card-body">
+                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
+                    <span className="font-weight-bold">Subtotal Price</span>
+                    <span>Rp {this.renderSubtotalPrice()}</span>
+                  </div>
+                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
+                    <span className="font-weight-bold">Tax Fee (5%)</span>
+                    <span>Rp {this.renderTaxFee()}</span>
+                  </div>
+                  <div className="d-flex my-2 flex-row justify-content-between align-items-center">
+                    <span className="font-weight-bold">Total Price</span>
+                    <span>Rp {this.renderTotalPrice()}</span>
+                  </div>
+                </div>
+                <div className="card-body border-top">
+                  <label htmlFor="recipientName">Recipient Name</label>
+                  <input onChange={this.inputHandler} type="text" className="form-control mb-3" name="recipientName"/>
+                  <label htmlFor="address">Address</label>
+                  <input onChange={this.inputHandler} type="text" className="form-control" name="address"/>
+                </div>
+                <div className="card-footer">
+                  <div className="d-flex flex-row justify-content-between align-items-center">
+                    <input onChange={this.inputHandler} name="payment" className="form-control mx-1" type="number"/>
+                    <button onClick={this.payBtnHandler} className="btn btn-success mx-1">Pay</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            : null
+          }
         </div>
       </div>
     )
